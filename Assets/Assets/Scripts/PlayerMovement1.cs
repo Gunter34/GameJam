fa4c4d35231fc43fb2ed51c724e0f9e652578d08
DESKTOP-99KM3IT\inter <interaktiv99@yandex.ru>
2023-04-30 21:17:29 +0300

    При перемещении Player скрытие/открытие платформ работает нормально

--- Assets/Assets/Scripts/PlayerMovement1.cs
+++ Assets/Assets/Scripts/PlayerMovement1.cs
@@ -4,27 +4,25 @@ using UnityEngine;
 
 public class PlayerMovement1 : MonoBehaviour
 {
-   public bool isGrounded;
+    private bool isGrounded;
+	private Rigidbody rb;
 
     void OnCollisionEnter()
     {		
 		isGrounded = true;
-		
     }
 
-
+    private void Awake()
+    {
+		rb = GetComponent<Rigidbody>();
+    }
     void Update()
     {
 					
 		if (isGrounded)
 		{
-			isGrounded=false;
-			GetComponent<Rigidbody>().AddForce(new Vector3(0,500,0));
+			isGrounded = false;
+			rb.AddForce(new Vector3(0,500,0));
 		}		
-		if (isGrounded = false)
-		{
-			isGrounded=false;
-			GetComponent<Rigidbody>().AddForce(new Vector3(0,-500,0));
-		}	
     }
 }
